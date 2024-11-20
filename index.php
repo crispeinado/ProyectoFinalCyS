3ff45aeebe569088a7c27b7ba4cf3a06c51ef5a6
crispeinado <cristinapeinado.dam@gmail.com>
2024-11-20 03:08:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+        font-size: 1.5em;
+        text-align: center;
+        margin-top: 200px;
+        font-weight: bold;
+        }
+        
+        body{
+        background-color: aquamarine;
+        }
+        </style>
+</head>
+<body>
+    <div>Bienvenido a la web de Cristina</div>
+    
+    <a href = "prueba_conexion.php">ir a la prueba de conexión con bd con php</a><br>
+    <a href = "practica_java">descargar el proyecto de tienda online con java y spring</a><br>
+</body>
+</html>
